b17018218492fb39072390d9de98142e8dc5fc28
SuvashiniDaliya <114674930+SuvashiniDaliya@users.noreply.github.com>
2022-09-29 20:17:37 +0600

    Update portfolios.js

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -37,11 +37,7 @@ export const portfolios = {
       name: "Dot Bangladesh",
       image: require("./image/hero/hero6.jpg"),
     },
-    {
-      type: "featured",
-      name: "Dot Bangladesh",
-      image: require("./image/hero/293713824_1084636635474416_4812242495996815638_n.jpg"),
-    },
+ 
   ],
   Logo: [
     {
